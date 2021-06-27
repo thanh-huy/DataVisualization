e397e18e3e51b7d64886b5271da57a0927757fbb
thanh-huy <66213437+thanh-huy@users.noreply.github.com>
2021-06-28 01:31:34 +0700

    Add files via upload

---
+++ test.R
@@ -0,0 +1,38 @@
+library(dslabs)
+data("heights")
+heights$height
+summary(heights$height) # ham tong ket cho gia tri min, max va cac luong tu (quartiles) trong mot vector
+#Tim phan tram percentiles theo cac luong tu
+p <- seq(0.01, 0.99, 0.01) # tao chuoi lap tu 0.01 den 0.99
+percentiles <- quantile(heights$height, p)
+#truy cap den gia tri cua 25% va 75%
+percentiles[names(percentiles)=="25%"] 
+percentiles[names(percentiles)=="75%"]
+#test qnorm(); qnorm la ham nghich dao cua pnorm()
+theoretical_quantiles <- qnorm(p, 69, 3)
+
+#Code 1.3
+library(tidyr)
+library(dslabs)
+data("heights")
+index <- heights$sex="Male" #lay index cua gioi tinh Nam
+x <- heights$height[index] #loc chieu cao gioi tinh Nam
+z <- scale(x) # lam smooth, tinh ti le co gian
+
+#ti le du lieu duoi 69.5
+mean(x<=69.5)
+
+#tinh toan luong tu quan sat duoc va luong tu ly thuyet
+p <- seq(0.05, 0.95, 0.05)
+observed_quantiles <- quantile(x,p)
+theoretical_quantiles <- qnorm(p, mean=mean(x), sd = sd(x))
+
+#ve voi QQ-plot
+plot(theoretical_quantiles,observed_quantiles)
+abline(0,1)
+
+#Tao QQ-plot voi gia tri lam smooth z
+observed_quantiles <- quantile(z,p)
+theoretical_quantiles <- qnorm(p)
+plot(theoretical_quantiles, observed_quantiles)
+abline(0,1)
